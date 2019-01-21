a9a8844bd3f3fc640cf57a58878b24441376db3c
Jason Douglas <jasond@me.com>
2019-01-20 17:29:06 -0800

    Decorate Multi objects returned by client

--- src/index.js
+++ src/index.js
@@ -15,19 +15,38 @@ AsyncRedis.createClient = (...args) => new AsyncRedis(args);
 const commandsToSkipSet = new Set(['multi']);
 // this is the set of commands to promisify
 const commandSet = new Set(commands.filter(c => !commandsToSkipSet.has(c)));
+// this is the set of commands that return a Multi
+const queueCommandSet = new Set(['batch', 'multi']);
+// this is the set of Multi commands to promisify
+const multiCommandSet = new Set(['exec', 'exec_atomic']);
+
+const promisify = function (object, method) {
+  return (...args) => new Promise((resolve, reject) => {
+    args.push((error, ...results) => {
+      if (error) {
+        reject(error, ...results);
+      } else {
+        resolve(...results);
+      }
+    });
+    method.apply(object, args);
+  });
+}
 
 AsyncRedis.decorate = redisClient => objectDecorator(redisClient, (name, method) => {
   if (commandSet.has(name)) {
-    return (...args) => new Promise((resolve, reject) => {
-      args.push((error, ...results) => {
-        if (error) {
-          reject(error, ...results);
-        } else {
-          resolve(...results);
+    return promisify(redisClient, method);
+  } else if (queueCommandSet.has(name)) {
+    return (...args) => {
+      // Decorate the Multi object
+      const multi = method.apply(redisClient, args);
+      return objectDecorator(multi, (name, method) => {
+        if (multiCommandSet.has(name)) {
+          return promisify(multi, method);
         }
+        return method;
       });
-      method.apply(redisClient, args);
-    });
+    }
   }
   return method;
 });
